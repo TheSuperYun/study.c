10008094fb15433aa49a78aa91bcb3cbd1aad2f3
TheSuperYun <134270342+TheSuperYun@users.noreply.github.com>
2023-10-27 20:45:40 +0800

    Add files via upload

---
+++ 最大和最小.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	int arr[10] = { 0 };
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	int max = arr[0], min = arr[0];
+	for (i = 1; i < 10; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+	}
+	printf("%d", max - min);
+	return 0;
+}
